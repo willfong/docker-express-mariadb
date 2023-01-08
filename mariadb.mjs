3609c64d5ce0cb58d9663d282783772f8a713447
Will Fong <willfong@hey.com>
2023-01-08 22:01:56 +0800

    Adding ESLint and Prettier

--- mariadb.mjs
+++ mariadb.mjs
@@ -1,24 +1,24 @@
 import mariadb from "mariadb";
 
 const pool = mariadb.createPool({
-	host: process.env.MARIADB_HOST,
-	port: process.env.MARIADB_PORT,
-	user: process.env.MARIADB_USER,
-	password: process.env.MARIADB_PASS,
-	connectionLimit: process.env.MARIADB_CONN,
+  host: process.env.MARIADB_HOST,
+  port: process.env.MARIADB_PORT,
+  user: process.env.MARIADB_USER,
+  password: process.env.MARIADB_PASS,
+  connectionLimit: process.env.MARIADB_CONN,
 });
 
 export async function query(query, args) {
-	let conn;
-	try {
-		conn = await pool.getConnection();
-		const res = await conn.query(query, args);
-		return res;
-	} finally {
-		if (conn) conn.release();
-	}
+  let conn;
+  try {
+    conn = await pool.getConnection();
+    const res = await conn.query(query, args);
+    return res;
+  } finally {
+    if (conn) conn.release();
+  }
 }
 
 export function disconnect() {
-	pool.end();
+  pool.end();
 }
