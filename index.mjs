c8f832d36b73e68bfda7f94918cd71bb5f000b59
Will Fong <willfong@hey.com>
2023-01-08 22:03:21 +0800

    Adding ESLint and Prettier (#2)

--- index.mjs
+++ index.mjs
@@ -9,36 +9,36 @@ const app = express();
 app.use(morgan("combined"));
 
 app.get("/", (req, res) => {
-	res.send("Hello World");
+  res.send("Hello World");
 });
 
 app.get("/alb-health-check", (req, res) => {
-	res.send(new Date());
+  res.send(new Date());
 });
 
 app.get("/delete-me-mariadb-test", async (req, res) => {
-	const query = "SELECT Host, User FROM mysql.user";
-	const results = await DB.query(query);
-	console.log("\n\nThe raw results object:");
-	console.log(results);
-	console.log("\nLettuce iterate!");
-	for (const row of results) {
-		console.log(`Found: ${row.User}@${row.Host}`);
-	}
-	res.json(results);
+  const query = "SELECT Host, User FROM mysql.user";
+  const results = await DB.query(query);
+  console.log("\n\nThe raw results object:");
+  console.log(results);
+  console.log("\nLettuce iterate!");
+  for (const row of results) {
+    console.log(`Found: ${row.User}@${row.Host}`);
+  }
+  res.json(results);
 });
 
 app.get("/error", (req, res) => {
-	throw new Error("This was intentionally thrown.");
+  throw new Error("This was intentionally thrown.");
 });
 
 app.use((req, res) => {
-	res.status(404).send("404 - File not found. Try again!\n");
+  res.status(404).send("404 - File not found. Try again!\n");
 });
 
 app.use((error, req, res, next) => {
-	console.error(error);
-	res.status(500).send("500 - Internal server error. Sorry, my bad!\n");
+  console.error(error);
+  res.status(500).send("500 - Internal server error. Sorry, my bad!\n");
 });
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
