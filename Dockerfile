beb7b1b9d2ccbe972ee2264125641a48fa26be1f
Will Fong <willfong@hey.com>
2023-03-26 13:09:39 +0800

    Build frontend and api

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,18 @@
-FROM node:18-alpine
+FROM node:18 AS frontend
+WORKDIR /app
+COPY frontend-react/package.json frontend-react/yarn.lock /app/
+RUN yarn install
+COPY frontend-react/. /app
+RUN yarn build
 
+
+FROM node:18-alpine
 WORKDIR /app
 COPY api/package.json api/yarn.lock /app/
 RUN yarn install
 
 COPY api/. /app
+
+COPY --from=frontend /app/build/. /app/frontend/
 
 CMD ["yarn", "start"]
--- api/index.mjs
+++ api/index.mjs
@@ -1,36 +1,16 @@
 import express from "express";
 import morgan from "morgan";
 
-import * as DB from "./mariadb.mjs";
+import api from "./api.mjs";
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(morgan("combined"));
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.use("/api", api);
 
-app.get("/alb-health-check", (req, res) => {
-  res.send(new Date());
-});
-
-app.get("/delete-me-mariadb-test", async (req, res) => {
-  const query = "SELECT Host, User FROM mysql.user";
-  const results = await DB.query(query);
-  console.log("\n\nThe raw results object:");
-  console.log(results);
-  console.log("\nLettuce iterate!");
-  for (const row of results) {
-    console.log(`Found: ${row.User}@${row.Host}`);
-  }
-  res.json(results);
-});
-
-app.get("/error", (req, res) => {
-  throw new Error("This was intentionally thrown.");
-});
+app.use(express.static('frontend'))
 
 app.use((req, res) => {
   res.status(404).send("404 - File not found. Try again!\n");
